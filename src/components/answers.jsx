48e8a92db0d0212e70bd234877a1f15aa53de2d1
Your Name <you@example.com>
2021-06-19 13:30:57 +0100

    update the answers component to have 4 buttons

--- src/components/answers.jsx
+++ src/components/answers.jsx
@@ -1,43 +1,19 @@
 import React, { Component } from 'react';
-import {Draggable } from "gsap/all"; 
+//import {Draggable } from "gsap/all"; 
+//import Draggable from "gsap/Draggable"; 
 
+//one prop for name
+//and one prop boolean
 
 class AnswerButtons extends Component {
-    state = {
-        disabled: false
-      };
-
-    toggleDraggable = () =>
-    this.setState(prevState => ({ disabled: !this.state.disabled }));
-
     render() { 
-    const { disabled } = this.state;
+
     return (
       <div className="container">
-        <Draggable disabled={disabled} bounds="parent">
-          <div
-            style={{ width: 200 }}
-            className={!disabled ? "draggable" : null}
-          >
-            <h4 style={{ height: 20, userSelect: "none" }}>
-              {!disabled && "Drag Me"}
-            </h4>
-            <textarea disabled={!disabled} className="uk-textarea" />
-            <input disabled={!disabled} className="uk-input" />
-            <input
-              className="uk-checkbox	"
-              type="checkbox"
-              disabled={!disabled}
-            />
-            <br />
-            <button
-              className="uk-button uk-button-primary"
-              onClick={this.toggleDraggable}
-            >
-              {disabled ? "Enable" : "Disable"} Drag
-            </button>
-          </div>
-        </Draggable>
+        <button>this.props.answer[0]</button>
+        <button>this.props.answer[1]</button>
+        <button>this.props.answer[2]</button>
+        <button>this.props.answer[3]</button>
       </div>
     );
   };
