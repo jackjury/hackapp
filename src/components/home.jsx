a1e10c5477091bf6a206619772313910817fb4a6
Jack Barrett <66954530+JackBarrett711@users.noreply.github.com>
2021-06-19 12:29:24 +0100

    added components and routing for homepage and image page

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class HomePage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>HomePage!</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
