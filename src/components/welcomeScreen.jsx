c97ab235ec0c0466ceff0aac33cbd95752a04a2d
aaron-edmiston <acedmiston@yahoo.com>
2021-06-19 13:53:22 +0100

    welcome screen pic

--- src/components/welcomeScreen.jsx
+++ src/components/welcomeScreen.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import CubClub from './CubClub.png';
+import cubClub from './CubClub.png';
+
 
 class WelcomeScreen extends Component {
   state = {};
   render() {
     return (
       <div>
-        <image src={CubClub} alt="Welcome" />
+        <img src={cubClub} alt="Welcome" />
         <Link to="/image-game">Image Game</Link>
       </div>
     );
