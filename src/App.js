393a932031626cc478721d77a15ab0ecaeaf6475
Jack Jury <jackjury@Jacks-MacBook-Pro.local>
2021-06-19 09:19:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello App</h1>
+    </div>
+  );
+}
+
+export default App;
